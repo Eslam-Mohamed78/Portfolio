12c27f74fc8d7be7a240a9b53b080b8a3f540b40
Eslam-Mohamed78 <eslam31043206@f-eng.tanta.edu.eg>
2024-12-03 06:33:15 +0200

    CV button redirects to cv page

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,8 +92,9 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Eslam-Mohamed Backend (Nodejs & Nestjs).pdf"
-          download
+          href="https://drive.google.com/file/d/1qH7jetVBm5kvQhnhG3SFsyMNQWhEaJDU/view?usp=drive_link"
+          target="_blank"
+          // download
         >
           Download CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
